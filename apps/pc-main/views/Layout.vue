65d1ea0766fda0d7f29cab971f71c7c3cad52f66
guozeng <guozeng@xiaogj.com>
2022-11-25 18:02:23 +0800

    auto import

--- apps/pc-main/views/Layout.vue
+++ apps/pc-main/views/Layout.vue
@@ -1,10 +1,20 @@
 <template>
   <div>{{ authStore.token }}</div>
+  <div @click="go">跳转</div>
 </template>
 
 <script setup>
 import { useAuthStore } from '../store'
+
 const authStore = useAuthStore()
+
+const aa = ref('')
+
+const rt = useRouter()
+
+function go() {
+  rt.push('/')
+}
 
 setTimeout(() => {
   authStore.token = 'adsd111'
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { mpa, cert, alias, commonjs } from './vite-config'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mpa(), cert(), commonjs()],
+  plugins: [
+    vue(),
+    mpa(),
+    cert(),
+    commonjs(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+    }),
+  ],
   server: {
     host: true,
     https: true,
